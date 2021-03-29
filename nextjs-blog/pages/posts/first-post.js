5b6b439da97d77a4464af19b80296235ab81248a
lunaboona <hyaluna@gmail.com>
2021-03-29 20:09:54 -0300

    add Head to first-post

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First post!</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href='/'>
