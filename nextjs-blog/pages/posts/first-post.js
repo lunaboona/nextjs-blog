b33a4495abb409e813600123150dac6865285d59
lunaboona <hyaluna@gmail.com>
2021-03-29 20:07:00 -0300

    add image

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function FirstPost() {
   return (
@@ -9,6 +10,8 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
+      <Image src='/images/profile.jpg' height={144} width={144} alt='lunaboona'></Image>
+      <p>it me</p>
     </>
   );
 }
