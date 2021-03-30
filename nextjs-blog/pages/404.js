f96638cdf4692730434a2e92feea5d57d0dee564
lunaboona <hyaluna@gmail.com>
2021-03-30 20:18:03 -0300

    add 404

---
+++ nextjs-blog/pages/404.js
@@ -0,0 +1,10 @@
+export default function Custom404() {
+  return (
+    <h1>
+      404!!!!!!!!!
+      <br /> OOPSIE WOOPSIE!!
+      <br /> uwu YOU made a fucky wucky!!
+      <br /> a wittle fucko boingo!
+    </h1>
+  );
+}
